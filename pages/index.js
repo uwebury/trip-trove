fd55455a3f079f313297c73ab76ea25ed20b1be1
uwebury <142432719+uwebury@users.noreply.github.com>
2024-01-15 15:42:25 +0100

    Mapped list from array

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,28 @@
+import { initialTrips } from "@/lib/data";
+import Image from "next/image";
+
 export default function HomePage() {
   return (
     <div>
-      <h1>Hello from Next.js</h1>
+      <h1>TripTrove</h1>
+      <ul>
+        {initialTrips.map((trip) => (
+          <li key={trip.slug}>
+            {trip.destination}
+            <ul>
+              <li>{trip.start}</li>
+              <li>{trip.end}</li>
+
+              <Image
+                src={trip.image}
+                width={200}
+                height={200}
+                alt={trip.destination}
+              />
+            </ul>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
