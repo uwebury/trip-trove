8c7eb20f573e407f04cafe9f8d1e77da9bf122a6
markusgemeinder <190774@gmx.de>
2024-01-17 10:46:06 +0100

    Create NavigationButton component, install (+) button to HomePage

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import CardList from "@/components/CardList";
+import NavigationButton from "@/components/NavigationButton";
 
 export default function HomePage() {
   return (
     <>
       <h1>TripTrove</h1>
       <CardList />
+      <NavigationButton href="/create" letter="+" position="right" />
     </>
   );
 }
