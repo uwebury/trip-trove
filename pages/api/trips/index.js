6748ed41c7eebdbe97da8cb8a768303b50e442a7
uwebury <142432719+uwebury@users.noreply.github.com>
2024-01-18 14:38:34 +0100

    Add API route /api/trips

---
+++ pages/api/trips/index.js
@@ -0,0 +1,18 @@
+import dbConnect from "@/db/connect";
+import Trip from "@/db/models/Trips";
+
+export default async function handler(request, response) {
+  await dbConnect();
+
+  try {
+    if (request.method === "GET") {
+      const trips = await Trip.find();
+      return response.status(200).json(trips);
+    } else {
+      return response.status(405).json({ message: "Method not allowed" });
+    }
+  } catch (error) {
+    console.error(error);
+    return response.status(400).json({ error: error.message });
+  }
+}
