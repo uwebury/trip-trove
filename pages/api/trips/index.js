9ec832b936287c727dc3b06ce1822f4b94ce30b8
uwebury <142432719+uwebury@users.noreply.github.com>
2024-01-19 15:43:59 +0100

    Use if … else pattern in whole handler function

--- pages/api/trips/index.js
+++ pages/api/trips/index.js
@@ -7,17 +7,16 @@ export default async function handler(request, response) {
   try {
     if (request.method === "GET") {
       const trips = await Trip.find();
-      return response.status(200).json(trips);
-    }
-    if (request.method === "POST") {
+      response.status(200).json(trips);
+    } else if (request.method === "POST") {
       const tripData = request.body;
       await Trip.create(tripData);
-      return response.status(201).json({ status: "Trip added" });
+      response.status(201).json({ status: "Trip added" });
     } else {
-      return response.status(405).json({ message: "Method not allowed" });
+      response.status(405).json({ message: "Method not allowed" });
     }
   } catch (error) {
     console.error(error);
-    return response.status(400).json({ error: error.message });
+    response.status(400).json({ error: error.message });
   }
 }
