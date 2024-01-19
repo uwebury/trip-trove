e68e9f003e44d5b5435aa1a32b82022bd3f639ef
uwebury <142432719+uwebury@users.noreply.github.com>
2024-01-19 16:46:24 +0100

    Delete styled component import

--- pages/trips/[id]/index.js
+++ pages/trips/[id]/index.js
@@ -1,22 +1,5 @@
 import { useRouter } from "next/router.js";
 import useSWR from "swr";
-import styled from "styled-components";
-
-const ImageContainer = styled.div`
-  position: relative;
-  height: 15rem;
-`;
-
-const ButtonContainer = styled.section`
-  display: flex;
-  justify-content: space-between;
-  gap: 0.2rem;
-
-  & > * {
-    flex-grow: 1;
-    text-align: center;
-  }
-`;
 
 export default function DetailsPage() {
   const router = useRouter();
@@ -26,13 +9,6 @@ export default function DetailsPage() {
   const { data: trip, isLoading, error } = useSWR(`/api/trips/${id}`);
 
   if (!isReady || isLoading || error) return <h2>Loading...</h2>;
-
-  async function deleteTrip() {
-    await fetch(`/api/trips/${id}`, {
-      method: "DELETE",
-    });
-    router.push("/");
-  }
 
   return (
     <>
