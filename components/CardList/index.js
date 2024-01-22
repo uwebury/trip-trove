b2216b8936927d0fd54039dc7041be25bc8c3273
uwebury <142432719+uwebury@users.noreply.github.com>
2024-01-22 10:59:40 +0100

    Create Card component

--- components/CardList/index.js
+++ components/CardList/index.js
@@ -1,53 +1,11 @@
-import styled from "styled-components";
-import Image from "next/image";
 import useSWR from "swr";
-import { formatDate } from "@/lib/utils";
-
-const StyledCardList = styled.ul`
-  margin: 0 3rem;
-  padding: 0;
-  display: flex;
-  justify-content: center;
-  flex-flow: column wrap;
-`;
-
-const StyledCard = styled.li`
-  border: 1px solid #ddd;
-  border-radius: 8px;
-  padding: 16px;
-  margin-bottom: 16px;
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-  list-style: none;
-`;
+import Card from "../Card";
 
 export default function CardList() {
-  const { data, error, isLoading } = useSWR("/api/trips", { fallbackData: [] });
+  const { error, isLoading } = useSWR("/api/trips", { fallbackData: [] });
   if (error) return <div>Failed to load</div>;
 
   if (isLoading) return <div>Loading...</div>;
 
-  return (
-    <StyledCardList>
-      {data.map((trip) => (
-        <StyledCard key={trip._id}>
-          <h2>{trip.destination}</h2>
-          <div>
-            <p>
-              <strong>Start:</strong> {formatDate(trip.start)}
-            </p>
-            <p>
-              <strong>End:</strong> {formatDate(trip.end)}
-            </p>
-          </div>
-          <Image
-            src={trip.imageURL}
-            width={300}
-            height={200}
-            alt={trip.destination}
-          />
-          <p>More Details</p>
-        </StyledCard>
-      ))}
-    </StyledCardList>
-  );
+  return <Card />;
 }
