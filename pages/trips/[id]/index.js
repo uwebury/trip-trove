d2cce4df4666cafe7711cee4d5d6ea702f3f8e7a
uwebury <142432719+uwebury@users.noreply.github.com>
2024-01-23 15:58:13 +0100

    Add handleEditClick

--- pages/trips/[id]/index.js
+++ pages/trips/[id]/index.js
@@ -1,9 +1,26 @@
-import { useRouter } from "next/router.js";
+import { useRouter } from "next/router";
 import useSWR from "swr";
 import Image from "next/image";
 import { formatDate } from "@/lib/utils";
 import PackingList from "@/components/PackingList";
 import BackButton from "@/components/Button/BackButton";
+import styled from "styled-components";
+
+const StyledFormButton = styled.button`
+  min-width: 140px;
+  padding: 0.5rem;
+  background-color: ${({ $backgroundColor }) => $backgroundColor};
+  border: 2px solid #848484;
+  border-radius: 0.5rem;
+  font-size: inherit;
+  font-weight: bold;
+  cursor: pointer; // optional for better UX
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: flex-end;
+`;
 
 export default function DetailsPage() {
   const router = useRouter();
@@ -11,6 +28,10 @@ export default function DetailsPage() {
   const { id } = router.query;
 
   const { data: trip, isLoading, error } = useSWR(`/api/trips/${id}`);
+
+  const handleEditClick = () => {
+    router.push(`${id}/edit`);
+  };
 
   if (error) return <h2>Error, please try again later...</h2>;
   if (!isReady || isLoading) return <h2>Loading...</h2>;
@@ -28,6 +49,11 @@ export default function DetailsPage() {
           alt={trip.destination}
         />
       </p>
+      <ButtonContainer>
+        <StyledFormButton onClick={handleEditClick} $backgroundColor="#ffdbdb">
+          Edit
+        </StyledFormButton>
+      </ButtonContainer>
       <p>
         <strong>Notes:</strong> {trip.notes}
       </p>
