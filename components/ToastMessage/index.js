07ee2e880a7a1538644daa45fefa8e8dcedb303e
markusgemeinder <190774@gmx.de>
2024-01-24 09:17:49 +0100

    Create CancelEditMessage in ToastMessage component (not implemented to Form yet)

--- components/ToastMessage/index.js
+++ components/ToastMessage/index.js
@@ -2,22 +2,45 @@ import { toast } from "react-hot-toast";
 
 export const SaveChangesMessage = ({ onConfirm, onCancel }) => (
   <div>
-    <p>Are you sure you want to save changes?</p>
+    <p>Are you sure to save changes?</p>
+    <button
+      onClick={() => {
+        onConfirm();
+        toast.dismiss();
+      }}
+    >
+      Yes, save
+    </button>
+    <button
+      onClick={() => {
+        onCancel();
+        toast.dismiss();
+      }}
+    >
+      No, don't save
+    </button>
+  </div>
+);
+
+export const CancelEditMessage = ({ onConfirm, onCancel, originalData }) => (
+  <div>
+    <p>Are you sure to discard all changes without saving?</p>
+    <button
+      onClick={() => {
+        // Pass the original data to onCancel
+        onCancel(originalData);
+        toast.dismiss(); // Dismiss the toast after canceling
+      }}
+    >
+      Yes, discard
+    </button>
     <button
       onClick={() => {
         onConfirm();
         toast.dismiss(); // Dismiss the toast after confirming
       }}
     >
-      Save
-    </button>
-    <button
-      onClick={() => {
-        onCancel();
-        toast.dismiss(); // Dismiss the toast after canceling
-      }}
-    >
-      Cancel
+      No, don't discard
     </button>
   </div>
 );
